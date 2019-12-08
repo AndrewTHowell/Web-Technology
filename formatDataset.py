47373f30ff7a8381ce975036bc7a6444f9094c7f
AndrewTHowell <howelldrew99@gmail.com>
2019-12-08 14:18:37 +0000

    Books dataset ready to load

---
+++ formatDataset.py
@@ -0,0 +1,40 @@
+# Section: Import modules
+
+import json
+
+import pandas as pd
+
+from os.path import dirname, abspath
+
+# Section End
+
+bookIDCounter = 0
+bookIDs = []
+bookTitles = []
+authors = []
+genres = []
+
+dataset = "C://Users//howel//Desktop//booksummaries//booksummaries.txt"
+with open(dataset, "r", encoding="utf-8") as file:
+    lines = file.readlines()
+    for line in lines:
+        [_, _, bookTitle, _, _, genre, _] = line.split("\t")
+
+        if genre != "":
+            genre = list(json.loads(genre).values())
+        else:
+            genre = []
+
+        bookIDs.append(bookIDCounter)
+        bookTitles.append(bookTitle)
+        genres.append(genre)
+
+        bookIDCounter += 1
+
+books = pd.DataFrame(data={"bookID": bookIDs, "bookTitle": bookTitles,
+                           "bookGenres": genres})
+
+print(books)
+
+currentPath = dirname(abspath(__file__)) + "//books.json"
+books.to_json(path_or_buf=currentPath, orient='records')
