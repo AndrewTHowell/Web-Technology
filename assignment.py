63a40aac9c1d26b36e5dd44cb72ddbe488f282e7
AndrewTHowell <howelldrew99@gmail.com>
2019-12-12 11:29:02 +0000

    Log in and exit sorted

--- assignment.py
+++ assignment.py
@@ -21,8 +21,42 @@ ratings = pd.read_csv(CURRENTPATH+"//ratings.csv", index_col=0)
 
 # Section End
 
-print(books)
-print(ratings)
+# Section: Functions
+
+
+# Section End
+
+# Section: Main program
+
+exit = False
+while not exit:
+    print("*** User Menu ***")
+    userID = int(input("Enter User ID: "))
+
+    logout = False
+    while not logout:
+        print("\n*** User {0}: Menu ***".format(userID))
+        print("1. Do stuff")
+        print("8. Logout")
+        print("9. Exit")
+
+        menuChoice = int(input("\nEnter choice: "))
+        if menuChoice == 8:
+            logout = True
+        if menuChoice == 9:
+            logout = True
+            exit = True
+
+    if exit:
+        break
+
+    choice = input("\nExit or Log in? (E or L): ").upper()
+    if choice == "E":
+        exit = True
+    else:
+        print("\n")
+
+# Section End
 
 # Section: Save database
 
