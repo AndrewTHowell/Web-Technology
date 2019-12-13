e6f27d0ab9cee03d49807ebea563d8b5feac45b8
AndrewTHowell <howelldrew99@gmail.com>
2019-12-13 04:35:40 +0000

    Checkpoint

--- flaskPractice.py
+++ flaskPractice.py
@@ -1,32 +1,40 @@
 # Section: Flask app
 
-from flask import Flask, redirect, url_for, request, render_template
+from flask import Flask, redirect, url_for, request, render_template, flash, session
 app = Flask(__name__)
+app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
+
+def passwordCorrect(userID, password):
+    if userID == 17:
+        if password == "hello":
+            return True
+    return False
 
 @app.route('/user/')
-def userPage(userID):
-    return render_template('user.html', userID = userID)
-
-@app.route('/loginResponse/')
-def response(success):
-    if success:
-
-    return render_template('response.html',
-                            title = title,
-                            message)
+def userPage():
+    userID = session["userID"]
+    return render_template('user.html', userID=userID)
 
 @app.route('/login', methods = ['POST', 'GET'])
 def login():
     if request.method == 'POST':
         form = request.form
         userID = form["userID"]
-        password = form["password"]
-        if passwordCorrect(userID, password):
-            flash("Password accepted")
-            return redirect(url_for('user', userID = userID))
+        if userID.isdigit():
+            userID = int(userID)
+            password = form["password"]
+            if passwordCorrect(userID, password):
+                flash("Password accepted")
+                session["userID"] = userID
+                return redirect(url_for('userPage'))
+            else:
+                print("Password rejected")
+                flash("Password rejected")
+                return redirect(url_for('login'))
         else:
-            flash("Password rejected")
-
+            print("Invalid userID")
+            flash("Invalid userID")
+            return redirect(url_for('login'))
     else:
         return render_template('login.html')
 
