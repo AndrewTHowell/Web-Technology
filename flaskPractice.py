b45328c9f827ed0c97cc6d3d8909d83c174122b8
AndrewTHowell <howelldrew99@gmail.com>
2019-12-13 10:39:51 +0000

    Checkpoint

--- flaskPractice.py
+++ flaskPractice.py
@@ -2,6 +2,7 @@
 
 from flask import Flask, redirect, url_for, request, render_template, flash, session
 import os
+import pandas as pd
 
 app = Flask(__name__)
 app.secret_key = os.urandom(16)
@@ -16,35 +17,49 @@ def passwordCorrect(userID, password):
 def index():
     return render_template('index.html')
 
+@app.route('/books/')
+def books():
+    df = pd.DataFrame({'A': [0, 1, 2, 3, 4],
+                   'B': [5, 6, 7, 8, 9],
+                   'C': ['a', 'b', 'c--', 'd', 'e']})
+    return render_template('books.html', tables=[df.to_html(classes='data', header="true")])
+
 @app.route('/user/')
 def userPage():
     if "logged_in" in session:
         if session["logged_in"]:
             userID = session["userID"]
-            return render_template('user.html', userID=userID)
+            return render_template('user.html')
     else:
         session["logged_in"] = False
         flash("Please log in first")
         return redirect(url_for('index'))
 
-@app.route('/login/', methods = ['POST', 'GET'])
+@app.route('/logout/', methods=["POST"])
+def logout():
+    session["logged_in"] = False
+    session["userID"] = None
+
+    flash("Logged out")
+    return redirect(url_for('index'))
+
+@app.route('/login/', methods=["POST"])
 def login():
-    if request.method == 'POST':
-        form = request.form
-        userID = form["userID"]
-        if userID.isdigit():
-            userID = int(userID)
-            password = form["password"]
-            if passwordCorrect(userID, password):
-                session["logged_in"] = True
-                session["userID"] = userID
-                return redirect(url_for('userPage'))
-            else:
-                flash("Password rejected")
-                return redirect(url_for('index'))
+    form = request.form
+    userID = form["userID"]
+    if userID.isdigit():
+        userID = int(userID)
+        password = form["password"]
+        if passwordCorrect(userID, password):
+            session["logged_in"] = True
+            session["userID"] = userID
+            return redirect(url_for('userPage'))
         else:
-            flash("Invalid userID")
+            flash("Password rejected")
             return redirect(url_for('index'))
+    else:
+        flash("Invalid userID")
+        return redirect(url_for('index'))
 
 if __name__ == "__main__":
     app.run()
